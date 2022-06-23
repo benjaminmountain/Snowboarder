34911ca9dc620a4b54bb5378e0030c6a7c5d4768
Ben Mountain <mountainben8@gmail.com>
2022-06-22 20:11:05 -0400

    Add crash and finish detector

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float reloadTime = 0.5f;
+    [SerializeField] ParticleSystem crashDetector;
     private void OnTriggerEnter2D(Collider2D other){
         if (other.tag == "Ground") {
+            crashDetector.Play();
            Invoke("reloadScene", reloadTime);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float reloadTime = 2f;
+    [SerializeField] ParticleSystem finishDetector;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
+            finishDetector.Play();
             Invoke("reloadScene", reloadTime);
             
         }
